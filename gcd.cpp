b2266dc9e888a2981126b077eb56a759a93669bc
Tamrin Nahar Lam <137845267+tamrin342@users.noreply.github.com>
2023-09-24 17:27:21 +0600

    Add files via upload

---
+++ gcd.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#include <ctype.h>
+#define ll long long
+#define vll vector<long long>
+#define vp vector < pair<long long, long long > >
+using namespace std;
+int main()
+{ll n,t;
+cin>>t;
+while(t--)
+{
+    cin>>n;
+    if(n%2==0) cout<<n/2-1<<endl;
+    else cout<<n/2<<endl;
+}
+}
